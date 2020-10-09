e815fcda90c4e56001a09144011a9e26b6a527b7
Leandro <Leandro@DESKTOP-HK6U5DG.fritz.box>
2020-10-09 14:58:25 +0200

    test 4

--- src/helloWorld/HelloJavaFX.java
+++ src/helloWorld/HelloJavaFX.java
@@ -19,7 +19,7 @@ public class HelloJavaFX extends Application {
 		
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-		 primaryStage.setTitle("Hello, 1.0.1.1.1.1");
+		 primaryStage.setTitle("Hello, 1.0.1.1.1.1lajföal");
 		 int i = 0; int j = 0; 
 		 primaryStage.setScene(scene);
 		 primaryStage.show();

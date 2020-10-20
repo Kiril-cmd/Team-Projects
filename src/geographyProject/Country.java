1c144279a41314d49e5040d474127e6d34012df0
Kiril <Kiril@DESKTOP-96DEBKH>
2020-10-20 16:03:08 +0200

    Classes city and state

--- src/geographyProject/Country.java
+++ src/geographyProject/Country.java
@@ -5,10 +5,6 @@ import java.util.ArrayList;
 public class Country extends GovernedRegion{
 	String countryName;
 	
-<<<<<<< HEAD
-	ArrayList<State> states = new ArrayList<State>();
-	
-=======
 	ArrayList<State> states;
 	
 	public Country(String newCountryName) {
@@ -20,5 +16,5 @@ public class Country extends GovernedRegion{
 	public void addState (State newState) {
 		states.add(newState);
 	}
->>>>>>> branch 'main' of https://github.com/leandro-hoenen/Team-Projects.git
+
 }

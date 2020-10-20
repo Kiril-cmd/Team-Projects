cc0c75c63041e9763beee7c091de44ae5f81700d
Leandro <Leandro@DESKTOP-HK6U5DG.fritz.box>
2020-10-20 16:28:05 +0200

    Layout GUI implemented

--- src/geographyProject/GeoView.java
+++ src/geographyProject/GeoView.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -27,11 +28,14 @@ public class GeoView {
 	protected Button btnCountry = new Button("Country");
 	protected Button btnState = new Button("State");
 	protected Button btnCity = new Button("City");
-	protected ListView<String> geoList = new ListView<String>();
+	protected ListView<String> countryList = new ListView<String>();
 	// Center controls
 	protected TextField tfPopulation = new TextField();
 	protected TextField tfFormOfGovernment = new TextField();
 	protected TextField tfArea = new TextField();
+	protected ListView<String> stateList = new ListView<String>();
+	protected ListView<String> cityList = new ListView<String>();
+	protected TextArea taHistory = new TextArea();
 
 	public GeoView(Stage primaryStage, GeoModel model) {
 		this.stage = primaryStage;
@@ -43,6 +47,7 @@ public class GeoView {
 		BorderPane root = new BorderPane();
 		root.setTop(createDataControls());
 		root.setLeft(createLeftControls());
+		root.setCenter(createCountryView());
 		
 		// TO DO: set up scene
 		Scene scene = new Scene(root);
@@ -74,9 +79,9 @@ public class GeoView {
 		HBox leftControlBtns = new HBox();
 		leftControlBtns.getChildren().addAll(btnCountry, btnState, btnCity);
 		VBox leftControls = new VBox();
-		leftControls.getChildren().addAll(leftControlBtns, geoList);
+		leftControls.getChildren().addAll(leftControlBtns, countryList);
 		ObservableList<String> items = FXCollections.observableArrayList("Switzerland", "Germany", "USA", "Canada");
-		geoList.setItems(items);
+		countryList.setItems(items);
 		
 		
 		return leftControls;
@@ -93,8 +98,17 @@ public class GeoView {
 		// This method creates static label objects for the country view and add those to the countryRoot
 		for (int i = 0; i < lbListCountries.length; i++) {
 			lbListCountries[i] = new Label(countryLabelText[i]);
-			countryRoot.add(lbListCountries[i], 0, i++);
+			countryRoot.add(lbListCountries[i], 0, i + 1);
 		}
+		// Add remain objects to createCountryView
+		countryRoot.add(lbInformation, 0, 0);
+		countryRoot.add(tfFormOfGovernment, 1, 1);
+		countryRoot.add(tfArea, 1, 2);
+		countryRoot.add(stateList, 1, 3);
+		countryRoot.add(cityList, 1, 4);
+		countryRoot.add(lbHistory, 1, 5);
+		countryRoot.add(taHistory, 1, 6);
+
 		
 		
 		return countryRoot;

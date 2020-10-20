aaf22347a883236155a3a6cd1336fc5f8306e259
Leandro <Leandro@DESKTOP-HK6U5DG.fritz.box>
2020-10-20 15:52:15 +0200

    States and Countries

--- src/geographyProject/Country.java
+++ src/geographyProject/Country.java
@@ -3,7 +3,17 @@ package geographyProject;
 import java.util.ArrayList;
 
 public class Country extends GovernedRegion{
-	String name;
+	String countryName;
 	
-	ArrayList<State> states = new ArrayList<State>();
+	ArrayList<State> states;
+	
+	public Country(String newCountryName) {
+		this.countryName = newCountryName;
+		states = new ArrayList<State>();
+		
+	}
+	
+	public void addState (State newState) {
+		states.add(newState);
+	}
 }
